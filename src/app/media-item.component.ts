1768a1fef2b749389d63c8c92fc7dec334273789
wjrSI <56023942+wjraggett@users.noreply.github.com>
2022-09-09 17:50:46 +0100

    wip

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name = 'The Redemption';
+  wasWatched() {
+    return true
+  }
+}
